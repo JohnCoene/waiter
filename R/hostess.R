eee157727ed10fde089b7bdcbdde86b9e813384c
John Coene <jcoenep@gmail.com>
2019-12-10 10:02:52 +0100

    deprecate with_waiter arg, rename to center_page as it currently is no longer strictly true: one can use the hostess with waiter as overlay on plot where with_waiter would have to be FALSE.

--- R/hostess.R
+++ R/hostess.R
@@ -7,7 +7,10 @@
 #' @param preset A loading bar preset, see section below.
 #' @param text_color The color of the loading text.
 #' @param class CSS class.
-#' @param with_waiter Set to \code{FALSE} if used without \code{waiter}.
+#' @param with_waiter Deprecate in favour of \code{center_page}.
+#' @param center_page By default the hostess is centered in the middle
+#' of the screen, ideal when using it with waiter full screen, set to 
+#' \code{FALSE} to prevent that.
 #' @param ... Any other other advanced options to pass to the loaded
 #' see the \href{https://loading.io/progress/}{official documentation}.
 #' 
@@ -51,15 +54,24 @@ use_hostess <- function(){
 
 #' @rdname hostess
 #' @export
-hostess_loader <- function(id = "hostess", preset = NULL, text_color = "#FFFFFF", with_waiter = TRUE, class = "", ...){
+hostess_loader <- function(id = "hostess", preset = NULL, text_color = "#FFFFFF", center_page = TRUE, class = "", ..., with_waiter){
   if(id == "hostess")
     warning("Using default `id`", call. = FALSE)
+
+  # will remove at 0.1.0
+  if(!missing(with_waiter)){
+    warning("The `with_waiter` argument is deprecated in favour of `center_page`.")
+    
+    # don't break stuff...
+    if(is.logical(with_waiter))
+      center_page <- with_waiter
+  }
 
   if(!is.null(preset))
     if(!preset %in% hostess_presets)
       stop("Invalid preset, see `?hostess_spinner` for valid values", call. = FALSE)
 
-  if(with_waiter)
+  if(center_page)
     class <- paste(class, "hostess-center")
 
   style <- paste0("color:", text_color, ";")
