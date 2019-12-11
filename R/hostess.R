bfc812b11bb483aa366bfea6447abee99fa94605
John Coene <jcoenep@gmail.com>
2019-12-11 14:09:13 +0100

    add arguments to hostess to allow further customisation of loader + method to set loader (to be renamed\?). #23

--- R/hostess.R
+++ R/hostess.R
@@ -13,6 +13,17 @@
 #' it with waiter full screen, for the latter set to \code{TRUE}.
 #' @param min,max Minimum and maximum representing the starting and ending
 #' points of the progress bar.
+#' @param svg Either an svg path e.g.: \code{M10 10L90 10} or the path to a \code{.svg}
+#' file. Note that if passing the latter it must be made available to Shiny by
+#' placing it either in the \code{www} folder or using \link[shiny]{addResourcePath}.
+#' @param progress_type The progress type, either \code{stroke} or \code{fill}.
+#' Ther former traces the path of the \code{svg} while the latter fills it progressively.
+#' @param fill_direction,stroke_direction The direction which the progress bar should
+#' take. Wether \code{fill_direction} or \code{stroke_direction} is used depends on
+#' \code{progress_type}.
+#' @param fill_color,stroke_color The color to use for the progress bar. 
+#' Wether \code{fill_color} or \code{stroke_color} is used depends on 
+#' \code{progress_type}.
 #' @param ... Any other other advanced options to pass to the loaded
 #' see the \href{https://loading.io/progress/}{official documentation}.
 #' 
@@ -57,10 +68,14 @@ use_hostess <- function(){
 #' @rdname hostess
 #' @export
 hostess_loader <- function(id = "hostess", preset = NULL, text_color = "#FFFFFF", 
-  center_page = FALSE, class = "", min = 0, max = 100, ..., with_waiter){
+  center_page = FALSE, class = "", min = 0, max = 100, svg = NULL, 
+  progress_type = c("stroke", "fill"), fill_direction = c("ttb", "btt", "ltr", "rtl"),
+  stroke_direction = c("normal", "reverse"), fill_color = NULL, stroke_color = NULL, 
+  ..., with_waiter){
 
-  if(id == "hostess")
-    warning("Using default `id`", call. = FALSE)
+  progress_type <- match.arg(progress_type)
+  fill_direction <- match.arg(fill_direction)
+  stroke_direction <- match.arg(stroke_direction)
 
   # will remove at > 0.1.0
   if(!missing(with_waiter)){
@@ -89,6 +104,36 @@ hostess_loader <- function(id = "hostess", preset = NULL, text_color = "#FFFFFF"
     `data-max` = max,
     ...
   )
+
+  if(!is.null(svg)){
+    # check if ends in .svg
+    # if .svg use data-img otherwise use data-path
+    is_img <- grepl("\\.svg", svg)
+
+    opts <- list()
+
+    if(is_img)
+      opts[["data-img"]] <- svg
+    else
+      opts[["data-path"]] <- svg
+
+    # set direction based on progress_type: stroke or fill
+    if(progress_type == "fill")
+      opts[["data-fill-dir"]] <- fill_direction
+    else
+      opts[["data-stroke-dir"]] <- stroke_direction
+
+    # set color based on progress_type: stroke or fill
+    if(progress_type == "fill" && !is.null(fill_color))
+      opts[["data-fill"]] <- fill_color
+    else if(progress_type == "stroke" && !is.null(stroke_color))
+      opts[["data-stroke"]] <- stroke_color
+
+    # add fill type
+    opts[["data-type"]] <- progress_type
+    opts$tag <- loader
+    loader <- do.call(tagAppendAttributes, opts) 
+  }
 
   return(loader)
 }
@@ -216,10 +261,26 @@ Hostess <- R6::R6Class(
 #' \code{FALSE} to prevent that.
 #' @param min,max Minimum and maximum representing the starting and ending
 #' points of the progress bar.
+#' @param svg Either an svg path e.g.: \code{M10 10L90 10} or the path to a \code{.svg}
+#' file. Note that if passing the latter it must be made available to Shiny by
+#' placing it either in the \code{www} folder or using \link[shiny]{addResourcePath}.
+#' @param progress_type The progress type, either \code{stroke} or \code{fill}.
+#' Ther former traces the path of the \code{svg} while the latter fills it progressively.
+#' @param fill_direction,stroke_direction The direction which the progress bar should
+#' take. Wether \code{fill_direction} or \code{stroke_direction} is used depends on
+#' \code{progress_type}.
+#' @param fill_color,stroke_color The color to use for the progress bar. 
+#' Wether \code{fill_color} or \code{stroke_color} is used depends on 
+#' \code{progress_type}.
 #' @param ... Any other other advanced options to pass to the loaded
 #' see the \href{https://loading.io/progress/}{official documentation}.
     loader = function(preset = NULL, text_color = "#FFFFFF", center_page = FALSE, 
-      class = "", min = NULL, max = NULL, ...){
+      class = "", min = NULL, max = NULL, svg = NULL, progress_type = c("stroke", "fill"), 
+      fill_direction = c("ttb", "btt", "ltr", "rtl"), stroke_direction = c("normal", "reverse"), 
+      fill_color = NULL, stroke_color = NULL, ...){
+
+      if(!is.null(private$.loader))
+        return(private$.loader)
       
       # allow overriding min and max
       if(!is.null(min))
@@ -230,7 +291,25 @@ Hostess <- R6::R6Class(
 
       hostess_loader(id = private$.id, preset = preset, text_color = text_color, 
         center_page = center_page, class = class, 
-        min = private$.min, max = private$.max, ...)
+        min = private$.min, max = private$.max, svg = svg, progress_type = progress_type,
+        fill_direction = fill_direction, stroke_direction = stroke_direction, 
+        stroke_color = stroke_color, ...)
+    }
+  ),
+  active = list(
+#' @field set_loader Set a loader as created by \code{\link{hostess_loader}}.
+    set_loader = function(value){
+      if(missing(value)){
+        return(private$.loader)
+      } else {
+        # change id, min and max to match class
+        value$attribs$id <- private$.id
+        value$attribs[["data-min"]] <- private$.min
+        value$attribs[["data-max"]] <- private$.max
+        private$.loader <- value
+      }
+        
+      invisible(self)
     }
   ),
   private = list(
@@ -239,6 +318,7 @@ Hostess <- R6::R6Class(
     .started = FALSE,
     .min = 0,
     .max = 100,
-    .current_value = 0
+    .current_value = 0,
+    .loader = NULL
   )
 )
