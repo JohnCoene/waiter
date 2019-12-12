782fbe27735e48331235aa272a6b645f964eed1d
JohnCoene <jcoenep@gmail.com>
2019-12-12 19:58:22 +0100

    remove attendant; useless

--- inst/assets/attendant/custom.js
+++
@@ -1,8 +0,0 @@
-
-Shiny.addCustomMessageHandler('attendant-start', function(opts) {
-	window.instance = $(opts.selector).scheletrone();
-});
-
-Shiny.addCustomMessageHandler('attendant-stop', function(opts) {
-	window.instance = $(opts.selector).scheletrone('stopLoader');
-});
