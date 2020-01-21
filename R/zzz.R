568a301e70d3c7fbe08284c7262b5999e2f9af3f
JohnCoene <jcoenep@gmail.com>
2020-01-21 11:32:40 +0100

    improved message when RStudio < 1.2 is running. fixes #41

--- R/zzz.R
+++ R/zzz.R
@@ -5,13 +5,15 @@
   )
 }
 
+# warn if RStudio is running and below 1.2
 .onAttach <- function(libname, pkgname) {
   has_rstudio <- rstudioapi::isAvailable()
 
   if(has_rstudio){
     has_correct_version <- rstudioapi::isAvailable("1.2")
-    msg <- paste("You are running RStudio version", rstudioapi::getVersion(), 
-      ", waiter may not work in the viewer, upgrade to 1.2+ to ensure it does."
+    msg <- paste0("You are running RStudio version '", rstudioapi::getVersion(), 
+      "', waiter may not work correctly in the viewer: upgrade to the latest version",
+      "to ensure or open Shiny apps in your browser."
     )
 
     if(!has_correct_version)
