5a71994a8c4f3694d49337ac04d3bb3e456b1b17
JohnCoene <jcoenep@gmail.com>
2021-07-11 11:40:19 +0200

    remove sourcemap

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,5 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map'
+  mode: 'production'
 });
